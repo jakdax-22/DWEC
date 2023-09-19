08d5e1be0be16882117fb8a67da6714e351bd24a
jakdax-22 <enriqueiranzomartinez4@gmail.com>
2023-09-19 12:27:52 +0200

    19/09/2023

---
+++ TEMA_1/ejercicios/ejercicio_1/scripts/2.js
@@ -0,0 +1,6 @@
+'use strict';
+//Crea una alerta con el típico mensaje hola mundo
+var mensaje = prompt("Escribe tu nombre");
+document.write(" Hola "+ mensaje);
+// Al saltar la primera alerta aparecerá otra con el mensaje "Soy el primer script"
+document.write("Soy el primer script");
---
+++ TEMA_1/primerScript/scripts/script1.js
@@ -0,0 +1,3 @@
+alert ("Hola     caracola \n Adiós caraculo");
+console.log ("Hola     caracola \n Adiós caraculo");
+document.write ("<pre>Hola     caracola<br>Adiós     caraculo</pre>");
