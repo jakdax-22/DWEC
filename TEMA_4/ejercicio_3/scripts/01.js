f69fe10c03e69e1a4d7c063904b46cff8e072152
jakdax-22 <enriqueiranzomartinez4@gmail.com>
2023-10-17 12:07:18 +0200

    10/17/23 12:07:18

--- TEMA_4/ejercicio_3/scripts/01.js
+++ TEMA_4/ejercicio_3/scripts/01.js
@@ -3,10 +3,9 @@ let nombre = "";
 
 while (nombre !== "fin"){
     nombre=prompt("Dime un nombre");
-    array.push(nombre);
-    longitudes.push([nombre,nombre.length])
+    array.push([nombre,nombre.length])
 }
 array.pop();
 for (let i = 0; i < array.length;i++){
-    document.write(array[i][j]+" => "+array[i][j]+"<br>");
+    document.write(array[i][0]+" => "+array[i][1]+"<br>");
 }
