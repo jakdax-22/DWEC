4830fc665dfe1690fac410da968d0f523b1094ab
jakdax-22 <enriqueiranzomartinez4@gmail.com>
2023-10-17 11:59:28 +0200

    10/17/23 11:59:28

--- TEMA_4/ejercicio_1/scripts/01.js
+++ TEMA_4/ejercicio_1/scripts/01.js
@@ -1,13 +1,8 @@
-console.log(Math.PI.toFixed(2));
-let array = [];
-let coincidencias = [];
+let coincidencias = [0,0,0,0,0,0,0,0,0,0];
 for (let i = 0; i < 10000; i++){
-    array[i] = Math.floor(Math.random()*(10 -1)+1);
+    coincidencias [Math.floor(Math.random()*(10 -1)+1)]++;
 }
 
-for (let numero of array){
-    coincidencias [numero]++;
-}
 for (let i = 1; i < 10; i++){
     document.write ("Número "+i+" : "+coincidencias[i]+"<br>");
 }
