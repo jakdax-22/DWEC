d73f5fb95c6138692599b561bcb8fad5989fc37b
EnriqueIranzo <enriqueiranzomartinez4@gmail.com>
2024-01-16 08:54:54 +0100

    01/16/24 08:54:54

--- TEMA_6/Ejercicios/Ejercicio_5/scripts/01.js
+++ TEMA_6/Ejercicios/Ejercicio_5/scripts/01.js
@@ -2,7 +2,6 @@ window.addEventListener('load', () => {
     let texto = document.getElementById("tarea");
     let lista = document.getElementById("lista");
     document.getElementById("mas").addEventListener("click",(e)=>{
-        console.log(lista.childNodes);
         e.preventDefault();
         if (texto.value==""){
             texto.placeholder="Por favor, rellena el campo";
@@ -19,24 +18,25 @@ window.addEventListener('load', () => {
             elemento.appendChild(quitar);
             lista.appendChild(elemento);
             span.addEventListener('click',(e)=>{
-                let listaCopia = [...lista.childNodes].filter((l)=>{
+                lista.insertBefore(elemento,lista.firstChild);
+                /*let listaCopia = [...lista.childNodes].filter((l)=>{
                     return l !== e.target.parentNode;
                 });
                 if (listaCopia.length > 1){
                     if(listaCopia[0].nodeName=="#text")
                         listaCopia.shift();
                 }
-                listaCopia.unshift(e.target.parentNode);
+                listaCopia.unshift(e.target.parentNode);*/
                 /*Acuérdate que no se puede hacer porque childNodes es de solo lectura
                 lista.childNodes = listaCopia;
                 */
                 //console.log(listaCopia);
-                while(lista.firstChild){
+                /*while(lista.firstChild){
                     lista.removeChild(lista.firstChild);
                 };
                 listaCopia.map((l)=>{
                     lista.appendChild(l);
-                });
+                });*/
             });
             quitar.addEventListener('click',(e)=>{
                 e.preventDefault();
