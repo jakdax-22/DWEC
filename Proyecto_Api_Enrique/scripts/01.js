b03d142cb3cc711588821aef01b7899c3ee73653
EnriqueIranzo <enriqueiranzomartinez4@gmail.com>
2024-02-21 10:22:38 +0100

    02/21/24 10:22:38

--- Proyecto_Api_Enrique/scripts/01.js
+++ Proyecto_Api_Enrique/scripts/01.js
@@ -1,4 +1,5 @@
 window.addEventListener('load',() =>{
+    console.log(document.cookie);
     /*Función para hacer la consulta a la API y obtener los datos de las cartas de cada expansión*/
     const listadoExpansion = async (expansion) =>{
         const url = `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/sets/${expansion}?collectible=1&locale=esES`;
@@ -11,17 +12,20 @@ window.addEventListener('load',() =>{
         };
 
         try {
+            while (main.childNodes.length > 2){
+                //console.log("Hola");
+                main.removeChild(main.lastChild);
+            }
             const response = await fetch(url, options);
             const result = await response.json();
-            if (main.childNodes.length > 3)
-            main.removeChild(main.lastChild);
+            /* Descartado porque se hacía pesado
             const gifparrafo = document.createElement("div");
             gifparrafo.className = "gif";
             const gifimagen = document.createElement("img");
             gifimagen.src="../img/logo.gif";
             gifimagen.alt="Spinner Logo";
             gifparrafo.appendChild(gifimagen)
-            main.append(gifparrafo);
+            main.append(gifparrafo);*/
             return result;
         } 
         catch (error) {
@@ -242,12 +246,13 @@ window.addEventListener('load',() =>{
         })
     }
     //Funcion para el despliegue de las cartas en pantalla
-    function despliegueCartas (resultado,pagina,numcartas,cargando=true){
+    function despliegueCartas (resultado,pagina,numcartas){
                 //Esto simplemente es por si se ha pulsado una imagen del inicio, para dar una pantalla de carga, si se da a pasar de página me la cargo
                 //Me cargo el último hijo de main para que no se me acumule, en este caso es la pantalla de carga
                 //main.removeChild(main.lastChild);
                 //Creo el div del contenedor
-                if (main.childNodes.length >= 3){
+
+                while (main.childNodes.length > 2){
                     //console.log("Hola");
                     main.removeChild(main.lastChild);
                 }
@@ -264,7 +269,7 @@ window.addEventListener('load',() =>{
                 //PD: Me he tirado 3 horas con errores por no hacerle un parseInt a ncartas, por lo que me concatenaba
                 //En vez de sumar y me daba que resultado[i].img no estaba definido :(((
                     console.log(resultado);
-                for(let i = numcartas * pagina; i < Number.parseInt(ncartas) + (numcartas * pagina) && i < resultado.length ;i++){
+                for(let i = numcartas * pagina; i < parseInt(ncartas) + (numcartas * pagina) && i < resultado.length ;i++){
                     //Depuración
                     /*console.log(ncartas + (24 * pagina));
                     console.log(i);*/
@@ -290,7 +295,7 @@ window.addEventListener('load',() =>{
                 let contador = 1;
                 //Más depuración
                 //console.log(resultado.length);
-                while (resultado.length > ncartas * (contador - 1)){
+                while (resultado.length > parseInt(ncartas) * (contador - 1)){
                     //Creo un span
                     let spanfootergaleria = document.createElement("span");
                     //Le doy el valor de contador
@@ -302,7 +307,7 @@ window.addEventListener('load',() =>{
                     //Y por último le pongo un listener para que vuelva a llamar a esta función, pero con el campo cargando en false
                     spanfootergaleria.addEventListener('click',()=>{
                         let numeroCartas = sacarValorCookie("ncartas");
-                        despliegueCartas(resultado,spanfootergaleria.textContent - 1,numeroCartas,false);
+                        despliegueCartas(resultado,spanfootergaleria.textContent - 1,parseInt(numeroCartas));
                     });
                     contador++;
                 }
@@ -364,8 +369,8 @@ window.addEventListener('load',() =>{
                 //Siempre me carga esto antes del resto de cosas y por eso no me detecta nada, preguntarle a sergio
                 document.querySelector("select").addEventListener('change',(ev)=>{
                     document.cookie = "ncartas="+ev.target.value;
-                    let numeroCartas = sacarValorCookie("ncartas");
-                    despliegueCartas(resultado,pagina,numeroCartas,true);
+                    let numeroCartas = parseInt(sacarValorCookie("ncartas"));
+                    despliegueCartas(resultado,pagina,numeroCartas);
                 });
     }
     //Voy a ponerle un escuchador a la foto de arriba izquierda para recargar la página
@@ -385,9 +390,11 @@ window.addEventListener('load',() =>{
     }
     //Función para hacer el fetch de cada carta y meterlo en la galeria
     const generarDisposicionFavoritos = async (ids,galeria) => {
-            console.log(main.childNodes.length);
-            if (main.childNodes.length > 2)
-            main.removeChild(main.lastChild);
+            //console.log(main.childNodes.length);
+            while (main.childNodes.length > 2){
+                //console.log("Hola");
+                main.removeChild(main.lastChild);
+            }
             //Voy a poner un cargando porque va para largo
             const gifparrafo = document.createElement("div");
             gifparrafo.className = "gif";
@@ -413,7 +420,7 @@ window.addEventListener('load',() =>{
             try {
                 //Espero el fetch y meto los valores devueltos a la foto y la meto en la galería
                 const response = await fetch(url, options);
-                const result = await response.json();
+                const result =  await response.json();
                 foto.addEventListener('click',()=>{
                     mostrarCarta(result[0].cardId);
                 });
@@ -425,6 +432,169 @@ window.addEventListener('load',() =>{
                 console.error(error);
             }
         }
+    }
+    function finalizarJuego (divJuego){
+        //Limpiando...
+        while(divJuego.childNodes.length > 2)
+        divJuego.removeChild(divJuego.lastChild);
+        //Un buen memazo
+        const meme = document.createElement("img");
+        meme.src = "../img/oscar.png";
+        meme.alt="perromeme";
+        meme.className = "meme";
+        divJuego.appendChild(meme);
+        //saco el record de las cookies
+        let record = parseInt(sacarValorCookie(" record"));
+        //Me cargo el record para volver a usarlo si vuelve el usuario 
+        document.cookie = "record=0";
+        //creo un div donde va a ir la puntuación junto con una pequeña tabla de resultados
+        const resultadoJuego = document.createElement("p");
+        resultadoJuego.textContent = "Puntuación: "+record;
+        resultadoJuego.className = "resultadoJuego";
+        divJuego.appendChild(resultadoJuego);
+        const mensajeJuego = document.createElement("p");
+        mensajeJuego.className = "mensajeJuego";
+        //Distintas puntuaciones
+        if (record > 33){
+            mensajeJuego.textContent = "¡Leroooooy Jenkinnnssss!"
+        }
+        else if (record < 33 && record > 25){
+            mensajeJuego.textContent = "Eres un fuera de serie"
+        }
+        else if (record < 25 && record > 15){
+            mensajeJuego.textContent = "Sabes que te está mirando a ti ¿no?"
+        }
+        else if (record < 15 && record > 5){
+            mensajeJuego.textContent = "Bueno... supongo que no está mal"
+        }
+        else if (record < 5 && record >= 0){
+            mensajeJuego.textContent = "Por tu bien dedícate al parchís";
+        }
+        divJuego.appendChild(mensajeJuego);
+        //Boton para volver a jugar
+        const botonJuego = document.createElement("button");
+        botonJuego.className = "botonJuego";
+        botonJuego.textContent = "Volver a jugar";
+        botonJuego.addEventListener('click',()=>{
+            juego();
+        });
+        divJuego.appendChild(botonJuego);
+        main.appendChild(divJuego);
+
+    }
+    //Función para el juego de acertar la frase graciosa
+    function juego (){
+        let expansion = Math.floor(Math.random() * 5);
+                switch(expansion){
+                    case 0:
+                        expansion = "Ashes%20of%20Outland";
+                    break;
+                    case 1:
+                        expansion = "Forged%20in%20the%20Barrens";
+                    break;
+                    case 2:
+                        expansion = "Madness%20at%20the%20Darkmoon%20Faire";
+                    break;
+                    case 3:
+                        expansion = "Galakrond%27s%20Awakening";
+                    break;
+                    case 4:
+                        expansion = "Scholomance%20Academy";
+                    break;
+                }
+        listadoExpansion(expansion).then((resultado)=>{
+            //Limpio
+            /*while(divJuego.childNodes.length > 3)
+            divJuego.removeChild(divJuego.lastChild);*/
+            //Compruebo si existe la cookie record y si no la creo
+            if (!sacarValorCookie(" record"))
+                document.cookie = "record=0";
+            //Creo el div donde irá el juego
+            const divJuego = document.createElement("div");
+            //Le doy clase
+            divJuego.className = "divJuego";
+            //Creo su título y su descripción
+            const tituloJuego = document.createElement("h2");
+            tituloJuego.textContent = "¿De quién es esta frase graciosa?";
+            tituloJuego.className = "tituloJuego";
+            const descripcionJuego = document.createElement("p");
+            descripcionJuego.textContent = "Este juego trata de elegir cual de las siguientes cartas es la que tiene la frase de abajo";
+            descripcionJuego.className = "descripcionJuego";
+            //Las añado al div
+            divJuego.appendChild(tituloJuego);
+            divJuego.appendChild(descripcionJuego);
+            //Ahora viene lo gracioso, tengo que crear un contador para que avance a cada respuesta y del resultado tengo que quitar las cartas que ya estén
+            //Todo esto metido en un while y a su vez sacar dos objetos random de resultado en cada iteración con un for
+            //while (contador < 5){
+                let rand1 = Math.floor(Math.random()*resultado.length);
+                let rand2 = Math.floor(Math.random()*resultado.length);
+                //console.log(rand1);
+                //console.log(rand2);
+                //Que no se repitan
+                while (rand1 == rand2){
+                     rand1 = Math.floor(Math.random()*resultado.length);
+                     rand2 = Math.floor(Math.random()*resultado.length);
+                }
+                //Array para sacarlo random
+                let opciones = [rand1,rand2];
+                //Creo un elemento para cada opcion
+                const img1 = document.createElement("img");
+                img1.src = resultado[rand1].img;
+                //Accesible
+                img1.alt = resultado[rand1].name;
+                const img2 = document.createElement("img");
+                img2.src = resultado[rand2].img;
+                //Accesible
+                img2.alt = resultado[rand2].name;
+                //Saco de cual de los dos va a ser la frase
+                let frase = opciones[Math.floor(Math.random() * 2)];
+                //console.log(frase);
+                frase = resultado[frase].flavor;
+                //Creo el elemento que va a llevar la frase justo debajo de las imágenes
+                let pJuego = document.createElement("p");
+                pJuego.textContent = frase;
+                //Pongo un listener a las fotos para que según lo que se elija pase una cosa u otra 
+                img1.addEventListener("click",()=>{
+                    if (resultado[rand1].flavor == frase){
+                        let record = parseInt(sacarValorCookie(" record"));
+                        record++;
+                        document.cookie = "record="+record;
+                        alertify.success("Has acertado");
+                        juego();
+                    }
+                    else {
+                        alertify.error("Lo siento, has fallado");
+                        finalizarJuego(divJuego);
+                        //Llamo a la función que limpia el juego y muestra puntuación
+
+                    }
+                });
+                img2.addEventListener("click",()=>{
+                    if (resultado[rand2].flavor == frase){
+                        let record = parseInt(sacarValorCookie(" record"));
+                        record++;
+                        document.cookie = "record="+record;
+                        alertify.success("Has acertado");
+                        juego();
+                    }
+                    else {
+                        finalizarJuego(divJuego);
+                        alertify.error("Lo siento, has fallado");
+                    }
+                });
+                //Meto las imágenes en su contenedor
+                img1.className = "imgJuego";
+                img2.className = "imgJuego";
+                divJuego.appendChild(img1);
+                divJuego.appendChild(img2);
+                //Luego también le hago un append del párrafo
+                pJuego.className = "pJuego";
+                divJuego.appendChild(pJuego);
+                //Meto el div en el main
+                main.appendChild(divJuego);
+
+            //}
+        });
     }
     //Función para sacar resultados de una búsqueda parcial de caracteres
     const resultadosBusquedaParcial = async (nombre) =>{
@@ -453,8 +623,10 @@ window.addEventListener('load',() =>{
     let main = document.querySelector("main");
     //Le pongo un listener para que cuando se pulse aparezca el listado de las cartas favoritas
     document.querySelector(".favoritos").addEventListener('click',()=>{
-        if (main.childNodes.length >= 3)
+        while (main.childNodes.length > 2){
+            //console.log("Hola");
             main.removeChild(main.lastChild);
+        }
         //Sacar un mensaje si no hay favoritos
         if(!sacarValorCookie(" mazo")){
             //console.log("funciona");
@@ -516,22 +688,33 @@ window.addEventListener('load',() =>{
                 }
                 //Llamo a la función y manejo la promesa
                 listadoExpansion(expansion).then(resultado=>{
-                    let numeroCartas = sacarValorCookie("ncartas");
-                    console.log(numeroCartas);
+                    let numeroCartas = parseInt(sacarValorCookie("ncartas"));
+                    //console.log(numeroCartas);
                     if (!numeroCartas){
                         document.cookie = "ncartas=24";
                         numeroCartas = 24;
                     }
-                    console.log(numeroCartas);
-                    despliegueCartas(resultado,0,numeroCartas,true);
+                    //console.log(numeroCartas);
+                    despliegueCartas(resultado,0,numeroCartas);
                 });
+            }
+            else if (imagen.id == "im5"){
+                //Limpiando
+                while (main.childNodes.length > 2)
+                    main.removeChild(main.lastChild);
+                //Si se pulsa el botón del cuestionario inicio el juego
+                //console.log("Hola");
+                juego();
+
             }
         });
         //Le pongo un escuchador a la lupa para que aparezca el buscador en el main
 
         document.getElementById("buscador").addEventListener("click",()=>{
-            if (main.childNodes.length > 2)
+            while (main.childNodes.length > 2){
+                //console.log("Hola");
                 main.removeChild(main.lastChild);
+            }
             //Ver si funciona, no sé por qué saca 6 Holas, preguntar a Sergio
             //console.log("Hola");
             if (main.childNodes.length == 2){
